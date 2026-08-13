2cbd2d6b110afcec65965ce87dd2779afd45d2b4
misodaxi <misodaxi.rretor@gmail.com>
2026-08-13 15:02:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`🚀 Casino 3D Servidor Multijugador activo en http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Casino 3D Servidor Multijugador activo en puerto ${PORT}`);
 });
