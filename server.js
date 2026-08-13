7cef8e41c9429c30cfb8982435d3a80e2da4f1b1
misodaxi <misodaxi.rretor@gmail.com>
2026-08-13 13:10:39 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'casino.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Multiplayer player tracking
